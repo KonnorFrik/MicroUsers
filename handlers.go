1a1b6d9e6b6b0b83db3e7176b2ccc438314accc9
Konnor <konnor.frik666@gmail.com>
2024-10-27 20:13:14 +0500

    Rename RedisDB to Cache

--- handlers.go
+++ handlers.go
@@ -10,7 +10,7 @@ func HandlerHello(ctx *gin.Context) {
 }
 
 func HandlerPing(ctx *gin.Context) {
-    pong, err := RedisClient.Ping()
+    pong, err := Cache.Ping()
 
     if err != nil {
         ctx.String(http.StatusInternalServerError, err.Error())
@@ -20,3 +20,80 @@ func HandlerPing(ctx *gin.Context) {
     ctx.String(http.StatusOK, pong)
 }
 
+// Allow info - name, email, password
+func HandlerRegister(ctx *gin.Context) {
+    var user UserDB
+    err := ctx.ShouldBindJSON(&user)
+
+    if err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    newPassword, err := CryptPassword(user)
+
+    if err != nil {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    user.Password = newPassword
+    newId, err := DbClient.CreateNewUser(user)
+
+    if err != nil {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    newToken, err := GenerateToken()
+
+    if err != nil {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    err = Cache.Save(newToken, newId, 0)
+    
+    if err != nil {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    ctx.JSON(http.StatusOK, gin.H{"token": newToken})
+}
+
+// For login take only email (it's unique)
+// Token may not be in cache, (expired or lost)
+//  so need to create new one and save
+func HandlerLogin(ctx *gin.Context) {
+    var user UserDB
+    err := ctx.ShouldBindJSON(&user)
+
+    if err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+}
+
+func HandlerGetByToken(ctx *gin.Context) {
+    ctx.JSON(http.StatusOK, gin.H{"error": "Not implemented"})
+}
+
+func HandlerGetByEmail(ctx *gin.Context) {
+    var record map[string]string
+    err := ctx.ShouldBindJSON(&record)
+
+    if err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    user, err := DbClient.GetUserByEmail(record["email"])
+
+    if err != nil {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    ctx.JSON(http.StatusOK, user)
+}
--- main.go
+++ main.go
@@ -17,12 +17,6 @@ import (
 const (
     DEFAULT_HTTP = 8000
     DEFAULT_HTTPS = 4430
-
-    // name must be same as in the compose file, links section
-    REDIS_DEFAULT_IP = "redis"
-    REDIS_DEFAULT_PORT = 6379
-    REDIS_DEFAULT_PASSWORD = ""
-    REDIS_DEFAULT_DB = 0
 )
 
 var (
@@ -33,16 +27,21 @@ var (
     server_port_https int
     server_address_https string
 
-    RedisClient *RedisDB
-    redisAddres string
+    Cache *RedisCache
     DbClient *DB
 )
 
 func main() {
     router := gin.Default()
 
+    // Just for test
     router.GET("/hello", HandlerHello)
     router.GET("/ping", HandlerPing)
+
+    router.POST("/user/register", HandlerRegister)
+    router.POST("/user/login", HandlerLogin)
+    router.POST("/user/token", HandlerGetByToken)
+    router.POST("/user/email", HandlerGetByEmail)
 
     router.Run(server_address_http)
 }
@@ -54,13 +53,14 @@ func init() {
     flag.IntVar(&server_port_http, "http", DEFAULT_HTTP, "http port for listen")
     flag.IntVar(&server_port_https, "https", DEFAULT_HTTPS, "https port for listen")
     // TODO: add flags for custom redis settings
+    // TODO: add flags for custom postgres settings
     flag.Parse()
 
     server_address_http = fmt.Sprintf("%s:%d", server_ip, server_port_http)
     server_address_https = fmt.Sprintf("%s:%d", server_ip, server_port_https)
-    redisAddres = fmt.Sprintf("%s:%d", REDIS_DEFAULT_IP, REDIS_DEFAULT_PORT)
+    redisAddres := fmt.Sprintf("%s:%d", REDIS_DEFAULT_IP, REDIS_DEFAULT_PORT)
 
-    RedisClient = RedisDBNew().Connect(redisAddres, REDIS_DEFAULT_PASSWORD, REDIS_DEFAULT_DB)
+    Cache = RedisCacheNew().Connect(redisAddres, REDIS_DEFAULT_PASSWORD, REDIS_DEFAULT_DB)
     DbClient = DBNew()
     err := DbClient.Connect(DB_DEFAULT_HOST, DB_DEFAULT_USER, DB_DEFAULT_PASSWORD, DB_DEFAULT_DBNAME, DB_DEFAULT_PORT, DB_DEFAULT_SSLMODE, DB_DEFAULT_TIMEZONE)
 
@@ -68,11 +68,12 @@ func init() {
     log.Printf("Server http  address: '%s'\n", server_address_http)
     log.Printf("Server https address: '%s'\n", server_address_https)
     log.Printf("Redis DB     address: '%s'\n", redisAddres)
-    log.Printf("Redis DB  client ptr: '%p'\n", RedisClient.client)
+    log.Printf("Redis DB  client ptr: '%p'\n", Cache.client)
 
     if err != nil {
         log.Printf("DB connect ERROR: '%s'\n", err.Error())
     } else {
         log.Printf("DB connect Success\n")
+        DbClient.DB.AutoMigrate(&UserDB{})
     }
 }
